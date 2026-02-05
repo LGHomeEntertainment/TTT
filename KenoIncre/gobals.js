95b3f81ba8e42bbd20b5276d70abb47c4ddd3e08
LGHomeEntertainment <candyleungmy@gmail.com>
2026-02-05 09:45:52 +0800

    Add global constants and variables for Keno game
    Initialized global constants and variables for Keno game.

---
+++ KenoIncre/gobals.js
@@ -0,0 +1,46 @@
+// ==== GLOBALs + CONSTANTS ====
+
+let playerPicks = [];
+// let playerMoney = 0;
+let bet = 1;
+let gameRunning = false;
+let generatedNumbers = [];
+
+// ==== UPGRADABLES ==== (those in brackets are KIV)
+let winningMultiplier = 1; // each next one is 2x
+let roundInterval = 3000; // for between rounds each subsequent upgrade is -50. floor is 1000?
+//let drawInterval = 1000; // each subsequent upgrade is -50. floor is 200
+let baseDrawInterval = 1000; // for number
+let drawnNumbers = 10; // ++, max 20? increases the chance of winning
+let boardSize = 80; // -10 per upgrade, to 40? keep drawnnumbers as well? then much higher chance of winning
+
+// ==== AUTOMATION ====
+// let roundsUpgrade (now autoPlay) = false; // just one upgrade to continuous. then it allows for round interval upgrades
+// let autoPick = false; // player can choose own 10 numbers, or toggle on or off for auto pick 10 numbers for every round? or make it asshole-y and first upgrade is autopick but it's for every round, then next one is to keep the numbers from the previous round.
+// let autoCollect = false; // collects the round's currencyB
+
+// all the upgrades. Do not use let for every single upgrade
+// those above this should be removed eventually
+let upgrade_defs = {
+  autoPlay: {
+    type: "boolean", // can only be true or false
+    cost: 1,
+  },
+  fasterDraw: {
+    type: "level", // best to have a formula or something to scale up difficulty later
+    baseCost: 10,
+    costScaling: 1.5, // optional, but nice
+    maxLevel: 20,
+  },
+};
+
+// ==== NOTES ====
+// currencyA = winning $, unlocks upgrades. updated as gameState.currency.money
+// currencyB = points? for automation. can be autocollected
+// currencyC = gold? for prestiging
+
+// ==== SOUNDS ====
+const sfxDraw = new Audio("sounds/draw.ogg");
+const sfxHit = new Audio("sounds/hit.ogg");
+const sfxWin = new Audio("sounds/win.wav");
+const sfxLose = new Audio("sounds/lose.wav");
